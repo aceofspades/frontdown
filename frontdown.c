b00ef22873916e009e3f1552c8a501f71bd3ed28
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2012-12-15 01:25:45 +0100

    Show compiler informations if compiler == GCC

--- frontdown.c
+++ frontdown.c
@@ -5,6 +5,9 @@
 
 int main(void){
 	printf("Frontdown %s\n", FD_VERSION);
+	#ifdef __GNUC__
+		printf("Compiled: %s %s with gcc %d.%d.%d\n\n", __DATE__, __TIME__, __GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+	#endif
 	
 	return 0;
 }

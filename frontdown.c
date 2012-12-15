c947fd7d54362acf11f7abfe5f419823944fe8db
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2012-12-15 01:10:51 +0100

    Init

---
+++ frontdown.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "frontdown.h"
+
+int main(void){
+	printf("Frontdown %s\n", FD_VERSION);
+	
+	return 0;
+}
---
+++ frontdown.h
@@ -0,0 +1,7 @@
+#ifndef __FRONTDOWN_H__
+#define __FRONTDOWN_H__
+
+#define FD_VERSION "0.1"
+
+#endif
+	
